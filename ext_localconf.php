ad48b4b2e0b6693306505fde67b8c8e4cfcccda4
Michael B <michidrau@gmail.com>
2022-03-14 20:51:47 +0100

    [TASK] Sets pagets configuration for list element

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,6 +7,7 @@ defined('TYPO3') or die();
 
 (static function () {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:tinyclassifieds/Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig"');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:tinyclassifieds/Configuration/TsConfig/TCEForm.tsconfig"');
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['afterBuildingFinished'][1647202696]
         = \Nimmermaer\Tinyclassifieds\Hooks\Form\AuthorHandling::class;
